58217157504cd02bda550f02b2eabc816371456d
M.Shahbazi <mohsenshahbazi@gmail.com>
2021-06-22 21:13:46 +0430

    product template creation

---
+++ showProductInfo.php
@@ -0,0 +1,11 @@
+<div id="productInfo">
+        <div id="ptitle">
+            <span><?=$data['title'];?></span>
+        </div>  
+        <div id="pimage">
+            <img src="<?=$data['image'];?>" />
+        </div>
+        <div id="pprice">
+            <span> قیمت : <?=$data['price'];?> ریال </span>
+        </div>
+</div>
