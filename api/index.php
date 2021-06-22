ee710a1169bfe2153a61ea038afc2b9506737945
M.Shahbazi <mohsenshahbazi@gmail.com>
2021-06-23 00:15:24 +0430

    add pdf export for rest api ( getProductInfo method)

--- api/index.php
+++ api/index.php
@@ -2,10 +2,17 @@
 require_once 'vendor/autoload.php';
 use RestService\Server;
 require_once '../get.php';
+require_once '../tcpdf/tcpdf.php';
 
         Server::create('/')
             ->addGetRoute('getProductInfo/(.*)', function($v) {
-                $data = getProductDetails ($v) ; 
-                return $data;
+                $data = getProductDetails ($v) ;
+                $pdfTextTitle = "محصول : ".$data['title']." <hr> قیمت :".$data['price'];
+                $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+                $pdf->AddPage();
+                $pdf->SetFont('bnazanin','', 12);
+                $pdf->WriteHTML($pdfTextTitle, true, 0, true, 0);
+                $pdf->Image($data['image']);
+               return $pdf->Output();
             })
         ->run();
